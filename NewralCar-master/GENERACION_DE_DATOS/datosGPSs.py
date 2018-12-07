245c74725f2f42799036287e05cef920b3e846ca
lagossully <44487802+lagossully@users.noreply.github.com>
2018-12-07 01:40:17 -0300

    UPDATE
    con SQL y todo perritz

--- NewralCar-master/GENERACION_DE_DATOS/datosGPSs.py
+++ NewralCar-master/GENERACION_DE_DATOS/datosGPSs.py
@@ -1,15 +1,34 @@
 from faker import Faker
+import SQL
 from time import time 
+from random import random, choice, randint
+
+from configuraciones import *
+import psycopg2
+conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
+
+
+database=SQL.SQLMAGIC()
+"""
+CREATE TABLE GPS'S
+            id_auto interger referenes AUTOS(id_auto),
+            fecha varchar(100),
+            hora varchar(100),
+            latitud integer, 
+            longitud integer,
+            PRIMARY KEY(id_auto, fecha, hora);
+"""
 
 fake = Faker()
 tiempo = time()
 
 print("GPS'S")
-print("insert into choques (id_auto, fecha,hora, latitud, longitud) values")
+print("insert into choques (id_auto, fecha, hora, latitud, longitud) values")
 
+x = 0
 while (x<10):
-    print("({},'{}','{}','{}'),".format(x,fake.name(),fake.address(), fake.email(),fake.url()))
-    x = x+1
+    print("({},'{}','{}','{}','{}'),".format(x,fake.date(),fake.time(), randint(-53000000,-18000000), randint(-73000000,-68000000)))
+    x = x + 1
 print(")")
 
 final = time()- tiempo
